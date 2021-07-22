3dd93926375be8ae659e7bd2a95162691e399a24
sinnott74 <sinnott74@hotmail.com>
2021-07-22 15:12:19 +0000

    Deploying to gh-pages from @ sinnott74/blog@042c18213119aa8b443a656ad16f12b2099e2ffd 🚀

--- assets/404.42592a29.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./vendor.741606fd.js";import{P as t}from"./Page.0b917ae6.js";import{C as a,c as r,T as l}from"./index.1aa9976d.js";export default()=>e.createElement(t,{title:"404"},e.createElement(a,{raised:!0},e.createElement(r,null,"404"),e.createElement(l,null,"Sorry that page couldn't be found... :/")));
