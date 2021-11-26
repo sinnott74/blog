9e33d953f2d9ea981bb69e892397b65718b43814
Daniel Sinnott <daniel.sinnott@kablamo.com.au>
2021-11-26 14:14:38 +1100

    Add more socials to share to

--- packages/client/src/core/ui/ShareLinks/ShareLinks.tsx
+++ packages/client/src/core/ui/ShareLinks/ShareLinks.tsx
@@ -1,32 +1,58 @@
 import React, { FC } from "react";
 import { ReactComponent as Twitter } from "../../images/twitter.svg";
 import { ReactComponent as Facebook } from "../../images/facebook.svg";
+import { ReactComponent as Reddit } from "../../images/reddit.svg";
+import { ReactComponent as HackerNews } from "../../images/hackernews.svg";
+import { ReactComponent as LinkedIn } from "../../images/linkedin.svg";
 import { Inline } from "../Layout";
-import { Link } from "../Link/Link";
 
 interface ShareLinksProps {
     url: string;
     title: string;
 }
 
+const shareTargets = [
+    {
+        href: ({ url, title }: ShareLinksProps) =>
+            `https://twitter.com/intent/tweet?url=${url}&text=${title}`,
+        Icon: <Twitter title="Share to Twitter" />,
+    },
+    {
+        href: ({ url, title }: ShareLinksProps) =>
+            `https://www.facebook.com/sharer/sharer.php?u=${url}`,
+        Icon: <Facebook title="Share to Facebook" />,
+    },
+    {
+        href: ({ url, title }: ShareLinksProps) =>
+            `http://www.reddit.com/submit?url=${url}&title=${title}`,
+        Icon: <Reddit title="Share to reddit" />,
+    },
+    {
+        href: ({ url, title }: ShareLinksProps) =>
+            `https://news.ycombinator.com/submitlink?u=${url}&t=${title}`,
+        Icon: <HackerNews title="Share to HackerNews" />,
+    },
+    {
+        href: ({ url, title }: ShareLinksProps) =>
+            `https://www.linkedin.com/sharing/share-offsite/?url=${url}`,
+        Icon: <LinkedIn title="Share to LinkedIn" />,
+    },
+];
+
 export const ShareLinks: FC<ShareLinksProps> = ({ url, title }) => {
-    const encodedUrl = encodeURI(url);
+    const encodedUrl = encodeURIComponent(url);
+    const encodedTitle = encodeURIComponent(title);
     return (
         <Inline>
-            <Link
-                to={`https://twitter.com/intent/tweet?url=${encodedUrl}&text=${title}`}
-                target="blank"
-                rel={["noopener", "noreferrer"]}
-            >
-                <Twitter title="Share to Twitter" />
-            </Link>
-            <Link
-                to={`https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`}
-                target="blank"
-                rel={["noopener", "noreferrer"]}
-            >
-                <Facebook title="Share to Facebook" />
-            </Link>
+            {shareTargets.map(({ href, Icon }) => (
+                <a
+                    href={href({ url: encodedUrl, title: encodedTitle })}
+                    target="blank"
+                    rel="noopener noreferrer"
+                >
+                    {Icon}
+                </a>
+            ))}
         </Inline>
     );
 };
